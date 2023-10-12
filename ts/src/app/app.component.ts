d0de760f6b7d81acc7259342b89fe9806f852f81
Luis Gustavo Rodrigues Oliveira <lgrotrab@gmail.com>
2023-10-12 15:53:23 -0300

    feat: adiciona seção de resumo

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'DevChuva';
+
+  compactText: boolean = true;
+  allText: boolean = false;
+
+  changeText(): void {
+    this.compactText = !this.compactText;
+    this.allText = !this.allText;
+  }
 }
